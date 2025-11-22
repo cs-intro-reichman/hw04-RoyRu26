ed77d7100afd737c77874409484e9286b1be1c0b
RoyRu26 <roy.rutzky@gmail.com>
2025-11-22 18:37:29 +0200

    completed primes and arrChar

--- Primes.java
+++ Primes.java
@@ -1,5 +1,28 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int N = Integer.parseInt(args[0]);
+        boolean[] isPrime = new boolean[N + 1];
+        int count = 0;
+        int primeNum = 0;
+        for (int i = 0; i < isPrime.length; i++) {
+            if(i > 1){
+                isPrime[i] = true;
+            }
+        }
+        while (count <= N) {
+            for (int i = 2; i < count; i++) {
+                if(count % i == 0){
+                    isPrime[count] = false;
+                }
+            }
+            count ++;
+        }
+        for (int i = 0; i < isPrime.length; i++) {
+            if(isPrime[i]){
+                primeNum ++;
+                System.out.println(i);
+            }
+        }
+        System.out.println("There are " + primeNum + " primes between 2 and " + N + " (" + (N * 10)/(primeNum - 1) + "%) are primes");
     }
 }
