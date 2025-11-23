3501c5c337db2b035b2f94748f6d9116c0e2d1d0
RoyRu26 <roy.rutzky@gmail.com>
2025-11-23 19:50:22 +0200

    mystring check

--- MyString.java
+++ MyString.java
@@ -18,13 +18,37 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        String low = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) < 91 && str.charAt(i) > 65) {
+                low += (char)(str.charAt(i) + 32);
+            }
+            else{
+                low += str.charAt(i);
+            }
+        }
+        return low;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
+        int count = 0;
+        for (int i = 0; i < str1.length(); i++) {
+            if(str1.charAt(i) == str2.charAt(0)){
+                for (int j = 0; j < str2.length(); j++) {
+                    if (str1.charAt(i) == str2.charAt(j)) {
+                        i ++;
+                        count ++;
+                    }
+                }
+            }
+            if(count == str2.length()){
+                return true;
+            }
+            else{
+                count = 0;
+            }
+        }
         return false;
     }
 }
