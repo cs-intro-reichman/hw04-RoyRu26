21e602af71fac8d8eeec4ec9eb05ada13d629d71
RoyRu26 <roy.rutzky@gmail.com>
2025-11-24 11:31:30 +0200

    fixed mystring

--- MyString.java
+++ MyString.java
@@ -1,7 +1,7 @@
 public class MyString {
     public static void main(String[] args) {
         System.out.println("Testing lowercase:");
-        System.out.println("UnHappy : " + lowerCase("UnHappy"));
+        System.out.println("UnHappy : " + lowerCase("unhappy"));
         System.out.println("This costs 15 Sheksls : " + lowerCase("This costs 15 Sheksls"));
         System.out.println("TLV : " + lowerCase("TLV"));
         System.out.println("lowercase : " + lowerCase("lowercase"));
@@ -20,7 +20,7 @@ public class MyString {
     public static String lowerCase(String str) {
         String low = "";
         for (int i = 0; i < str.length(); i++) {
-            if (str.charAt(i) < 91 && str.charAt(i) > 65) {
+            if (str.charAt(i) < 91 && str.charAt(i) > 64) {
                 low += (char)(str.charAt(i) + 32);
             }
             else{
@@ -29,24 +29,17 @@ public class MyString {
         }
         return low;
     }
-
+    //hellowrold
+    //     world
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        int count = 0;
-        for (int i = 0; i < str1.length(); i++) {
-            if(str1.charAt(i) == str2.charAt(0)){
-                for (int j = 0; j < str2.length(); j++) {
-                    if (str1.charAt(i) == str2.charAt(j)) {
-                        i ++;
-                        count ++;
-                    }
-                }
+        for (int i = 0; i <= str1.length() - str2.length(); i++) {
+            int j = 0;
+            while (j < str2.length() && str1.charAt(i + j) == str2.charAt(j)) {
+                j ++;
             }
-            if(count == str2.length()){
+            if (j == str2.length()) {
                 return true;
-            }
-            else{
-                count = 0;
             }
         }
         return false;
